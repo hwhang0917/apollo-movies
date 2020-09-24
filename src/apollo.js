cb9fb51dec7c0d50f39fe2e2053c88780b1351ae
Heesang whang <hwhang0917@gmail.com>
2020-09-24 23:08:53 +0900

    🔨 Add NODE_ENV for production

--- src/apollo.js
+++ src/apollo.js
@@ -1,7 +1,10 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000",
+  uri:
+    process.env.NODE_ENV === "production"
+      ? "https://movieql.netlify.app/graphql"
+      : "http://localhost:4000",
   cache: new InMemoryCache(),
 });
 
